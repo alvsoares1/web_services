434a10b7b753884aed0b47afa4710189a1af6d6a
alvsoares1 <alvaroass2110@gmail.com>
2024-01-18 14:55:02 -0300

    OrderStatus enum, Category, Product

---
+++ src/main/java/com/example/webservices/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.webservices.repositories;
+
+import com.example.webservices.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
